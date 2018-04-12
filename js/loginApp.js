f385ea8f104e245ef642a7f7c9f71c9db488b7f3
artjohn1993 <artjohn1993@gmail.com>
2018-04-12 10:21:52 +0800

    add admin registration

--- js/loginApp.js
+++ js/loginApp.js
@@ -1,20 +1,38 @@
 var app = angular.module('BasicHttpAuthExample', ['ngStorage']);
 app.controller('loginCtrl', function($scope,$localStorage,$sessionStorage,$http) {
-
+    $scope.accountExist = false;
+    $scope.accountRegistered = false;
+    $scope.invalidPassword = false;
     $scope.submit = function(){ 
           $localStorage.username =$scope.emailaddress;
           $localStorage.password =$scope.password;
-          
-
           $scope.checkLogin($localStorage.username, $localStorage.password);
+    }
+    $scope.register = function () {
+        var registerAdmin = { username: $scope.registerUsername, email: $scope.registerEmail, password: $scope.registerPassword };
+        console.log(registerAdmin);
+        $http.post('http://13.75.89.123/pooling/api/Auth/Registration', registerAdmin).then(
+            function (response) {
+                $scope.accountExist = false;
+                $scope.registerUsername = "";
+                $scope.registerEmail = "";
+                $scope.registerPassword = "";
+                $scope.accountRegistered = true;
+                $scope.accountExist = false;
+                $scope.invalidPassword = false;
 
-          //if ($localStorage.username == "admin" && $localStorage.password == "admin") {
-          //    window.location.href ="index.html";
-          //}else{
-          //    $localStorage.username = "";
-          //    $localStorage.password = "";
-          //    alert("Wrong email or password.")
-          //}
+            }, function (error) {
+                
+                if (error.data.Username) {
+                    $scope.invalidPassword = false;
+                    $scope.accountExist = true;
+                }
+                else if (error.data.Password) {
+                    $scope.accountExist = false;
+                    $scope.invalidPassword = true;
+                }
+               
+            })
     }
 
     $scope.checkLogin = function (uname, pass) {
